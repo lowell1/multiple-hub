2ebedf3c4dbd4ce1a92e55d71e44786c718253dc
lowell <29132034+lowell1@users.noreply.github.com>
2022-06-04 14:20:37 -0700

    add favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,18 @@
 import "../styles/global.css";
 import Nav from "../components/nav/Nav";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Multiple</title>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="images/multiple-favicon.png"
+        />
+      </Head>
       <Nav />
       <Component {...pageProps} />
     </>
